7d5d84c93e87a41426e9d3cc5cea6d4d4cc62fa2
Matia <matia.casu@gmail.com>
2021-05-07 19:59:52 +0200

    Sezione header con logo e nav

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Molisana Laravel</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+        <header>
+        <img class="logo" src="{{ asset('img/marchio-sito-test.png')}}" alt="">
+        <ul class="main-nav list-inline">
+            <li> <a href="">Home</a> </li>
+            <li> <a href="">Prodotti</a> </li>
+            <li> <a href="">News</a> </li>
+        </ul>
+        </header>
+        <main>
+            <div class="full-bg-container">
+
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </div>
+</body>
+</html>
